6bef64c4ae8e89a087e65d0e25bae56f9158a617
Priyanka7011 <pk3201134@gmail.com>
2022-01-01 21:33:29 +0530

    first commit

---
+++ anagrams.cpp
@@ -0,0 +1,30 @@
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        unordered_map<string,vector<string>> hash;
+        vector<vector<string>> ans;
+        
+        for(auto s:string_list){
+            string temp=s;
+            sort(s.begin(),s.end());
+            hash[s].push_back(temp);
+            
+        }
+        
+        for(auto m:hash){
+            vector<string> test;
+            for(auto x :m.second){
+                //cout<<x<<" ";
+                test.push_back(x);
+            }
+            //cout<<"\n";
+            ans.push_back(test);
+            test.clear();
+        }
+        
+        return ans;
+        
+    }
+};
